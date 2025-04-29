88aa069d1be1461c13e5725d8febe16505fde560
Artur Farrardo <dev.arturfarrardo@gmail.com>
2025-04-29 06:06:49 -0300

    Arrumado o login.js e redirecionamento do usuário
    Arrumado o login.js com a requisição da API funcionando e redirecionamento do usuário para a tela inicial do site após a realização do login.

---
+++ Opaline_DevWeb-main/README.md
@@ -0,0 +1,19 @@
+## Seja bem vindo ao Opaline_DevWeb!
+
+Documentação de api para a utilização do projeto: https://github.com/EveMerces/project-dw 
+
+🔧 Funcionalidades
+
+🔹 Página Inicial: Exibe produtos disponíveis na loja
+
+🔹 Sobre nós: Projeto de desenvolvimento web voltado para criar soluções intuitivas e eficientes, com foco em qualidade, segurança e performance.
+
+🔹 Cadastro de Usuário: Permite criar uma conta na plataforma
+
+Tecnologias Utilizadas no Desenvolvimento:
+
+| Linguagem   | Ícone  |
+|------------|--------|
+| HTML       | ![HTML](https://cdn.jsdelivr.net/gh/devicons/devicon/icons/html5/html5-original.svg) |
+| CSS        | ![CSS](https://cdn.jsdelivr.net/gh/devicons/devicon/icons/css3/css3-original.svg) |
+| JavaScript | ![JavaScript](https://cdn.jsdelivr.net/gh/devicons/devicon/icons/javascript/javascript-original.svg) |
---
+++ Opaline_DevWeb-main/cadastro.js
@@ -0,0 +1,73 @@
+async function save (){
+    let name = document.getElementById("nome").value;
+    if (name == ''){
+        alert("Digite o nome de usuário!")
+        return
+    }
+    let email = document.getElementById("email").value;
+    if (email == ''){
+        alert("Digite um endereço de email válido!")
+        return
+    }
+    let birthday = document.getElementById("datanasc").value;
+    if (birthday == ''){
+        alert("Insira uma data de nascimento válida!")
+        return
+    }
+    let password = document.getElementById("senha").value;
+    if (password == '' || password.length < 6 ){
+        alert("Digite uma senha válida!")
+        return
+    }
+    let CpfCnpj = document.getElementById("cpfcnpj").value;
+    if (CpfCnpj == '' || CpfCnpj.length < 11 ){
+        alert("Digite um CPF ou CNPJ válidos!")
+        return
+    }
+    let termos = document.getElementById("termos").checked;
+    if (termos == false){
+        alert("Por favor, aceite os termos de uso!")
+    }
+    let userType = 1
+    
+    dados = {
+          "name": name,
+          "email": email,
+          "user_type_id": userType,
+          "password": password,
+          "cpf_cnpj": CpfCnpj,
+          "terms": termos,
+          "birthday": birthday
+    }
+
+    let api = await fetch("https://go-wash-api.onrender.com/api/user",{
+      method: "POST",
+      body:JSON.stringify(dados),
+          headers: {'content-type':'application/json'}
+      });
+
+      if (api.ok){
+          let resp = await api.json();
+          console.log(resp);
+          alert("Usuário cadastrado!");
+          return
+      }else { 
+        let respError = await api.json();
+        console.log(respError)
+            if (respError.data.errors.email) {
+                alert('The email has already been taken.')
+            }
+
+            if (respError.data.errors.cpf_cnpj == null) {
+                alert('cpf_cppj invalid.')
+            } else {
+                alert('The cpf cnpj has already been taken.')
+            }
+    }
+  }
+
+
+function cadastro(){
+    save();
+    
+}
